d867907a81514a99887feb4a42d5b7ffc58dddcb
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-03-07 00:21:59 +0530

    Read middleware docs

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}) -> name('home');
+
+
+Route::get('/this', function(){
+    return "You are on This page";
+}) -> middleware('token.verified');
