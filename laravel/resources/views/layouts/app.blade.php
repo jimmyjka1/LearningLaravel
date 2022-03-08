9b87202c1b0d816114299371f2fcaa74725739c7
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-03-08 23:58:48 +0530

    updated npm and other things

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel App - @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
         integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> --}}
 
@@ -22,7 +22,17 @@
 </head>
 
 <body>
-    <div class="p-1">
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm mb-3">
+        <h5 class="my-0 me-auto font-weight-normal">Laravel App</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="text-decoration-none text-black p-2" href="{{ route('home.index') }}">Home</a>
+            <a class="text-decoration-none text-black p-2" href="{{ route('home.contact') }}">Contact</a>
+            <a class="text-decoration-none text-black p-2" href="{{ route('posts.index') }}">Blog Post</a>
+            <a class="text-decoration-none text-black p-2" href="{{ route('posts.create') }}">Add Blog</a>
+        </nav>
+    </div>
+    <div class="p-3">
         @if (session('status'))
             <div class="text-success">
                 {{ session('status') }}
